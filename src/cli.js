4543121e0464fd25ec92dcdfa3d686eb80ed9cef
Alex <san4ezz.8989@gmail.com>
2020-06-10 14:46:00 +0300

    fix mistake

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,6 @@ export default () => {
   console.log('Welcome to the Brain Games!');
 
   const name = readlineSync.question('May I have your name? ');
-    console.log("CI wrong");
+  console.log('CI wrong');
   console.log(`Hello, ${name}!`);
 };
