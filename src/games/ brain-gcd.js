cdc608390ab422d7b48815dc7dd1182fc9955f53
Alex <san4ezz.8989@gmail.com>
2020-06-25 16:25:14 +0300

    add new game brain-gcd

---
+++ src/games/ brain-gcd.js
@@ -0,0 +1,71 @@
+import {
+  getRandomInt,
+  gameEngine,
+} from '../index.js';
+
+const gameRulesText = 'Find the greatest common divisor of given numbers.';
+
+
+const buildQuestionExpr = () => {
+
+  const findAllDivisors = (num) => {
+    const minNum = 1;
+    let resNum = num;
+    const divisorsListForNumber = [];
+    const divisorsList = [2, 3, 4, 5, 6, 7, 8, 9];
+
+    const filterDividerTakeNum = (curNum) => (divisor) => (curNum % divisor === 0);
+
+    while (minNum !== resNum) {
+      const filterDivider = filterDividerTakeNum(resNum);
+      const [divisorForNum] = divisorsList.filter(filterDivider);
+      if (divisorForNum) {
+        divisorsListForNumber.push(divisorForNum);
+        resNum /= divisorForNum;
+      } else {
+        divisorsListForNumber.push(resNum);
+        resNum /= resNum;
+      }
+    }
+
+    return divisorsListForNumber;
+  };
+
+  const evalCorrectAnswer = (firstNum, secondNum) => {
+    const firstDividersList = findAllDivisors(firstNum);
+    const secondDividersList = findAllDivisors(secondNum);
+
+    const commonDividersList = firstDividersList.filter((divider) => {
+      const elmentIndex = secondDividersList.indexOf(divider);
+      if (elmentIndex !== -1) {
+        secondDividersList.splice(elmentIndex, 1);
+        return true;
+      }
+      return false;
+    });
+
+    if (commonDividersList.length) {
+      return commonDividersList.reduce((acc, divvider) => acc * divvider);
+    }
+    return 1;
+  };
+  const first = getRandomInt(1, 20);
+  const second = getRandomInt(1, 20);
+
+  const corrAnswer = evalCorrectAnswer(first, second);
+  const questionBudy = `${first} ${second}`;
+
+  return {
+    questionBudy,
+    corrAnswer,
+    isAnswerCorrect(answer) {
+      return answer === corrAnswer.toString();
+    },
+  };
+};
+
+const brainGcd = () => {
+  gameEngine(gameRulesText, buildQuestionExpr);
+};
+
+export default brainGcd;
