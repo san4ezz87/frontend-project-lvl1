ba2b216c33c4e4d3bcc8e6c2306f33110105cf96
Alex <san4ezz.8989@gmail.com>
2020-07-13 13:36:22 +0300

    delete variable for min prime number

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -3,15 +3,13 @@ import getRandomInt from '../utils/utils.js';
 
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (num) => {
-  const smalllerNum = 2;
-  const numAbs = Math.abs(num);
-  if (numAbs < smalllerNum) {
+  if (num < 2) {
     return false;
   }
 
-  const maxDivisor = Math.sqrt(numAbs);
+  const maxDivisor = Math.sqrt(num);
   for (let divisor = 2; divisor <= maxDivisor; divisor += 1) {
-    if (numAbs % divisor === 0) {
+    if (num % divisor === 0) {
       return false;
     }
   }
